eed18cef48b744a3d69593363cf3ce286e0e8137
Miguel Cid <miguel.cid.paz@gmail.com>
2022-05-10 21:28:04 +0200

    pop

--- src/components/Carrousel/index.jsx
+++ src/components/Carrousel/index.jsx
@@ -1,33 +1,35 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
 /* import { useState } from "react"; */
-import "swiper/css";
-import "swiper/css/pagination";
+import "swiper/scss";
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
+import 'swiper/scss/grid';
 import "./index.scss";
 
 const itineraries = [
     {urlImage:'https://img1.10bestmedia.com/Images/Photos/378847/GettyImages-1085317916_54_990x660.jpg',title:'Barcelona'},{urlImage:'https://a.cdn-hotels.com/gdcs/production133/d1207/7ad2d7f0-68ce-11e8-8a0f-0242ac11000c.jpg',title:'Madrid'},
     {urlImage:'https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900',title:'Paris'},{urlImage:'https://www.cadizturismo.com/storage/app/media/uploaded-files/p-cadiz_turismo.jpg',title:'Cadiz'}
 ]
-const imageSize = '100px'
+
 export function Carrousel (props){
     /* const itineraries = props.itineraries */
 
 
     return <>
     <Swiper
-    slidesPerView={3}
+    slidesPerView={4}
     spaceBetween={10}
-    slidesPerGroup={3}
+    slidesPerGroup={4}
     loop={true}
     loopFillGroupWithBlank={false}
     pagination={{
       clickable: true
     }}
     modules={[Pagination]}
-    className="mySwiper"
     >
-        {itineraries.map((e,i)=> <SwiperSlide key={i}><img width={imageSize} height={imageSize} src={e.urlImage} alt=''/><h3>{e.title}</h3></SwiperSlide>)}
+        {itineraries.map((e,i)=> <SwiperSlide className="swiper-slider" key={i}><img className="carrousel--img-slider" src={e.urlImage} alt='event'/><h3>{e.title}</h3></SwiperSlide>)}
     </Swiper></>
 
 }
