1f44b9843ffdcb7564c096dca8fea890949379cd
Miguel Cid <miguel.cid.paz@gmail.com>
2022-05-14 14:27:18 +0200

    finalizado js?

--- src/components/Discover/index.jsx
+++ src/components/Discover/index.jsx
@@ -1,17 +1,30 @@
 import { Carrousel } from "../Carrousel"
+import { useState, useEffect } from 'react'
+import { callToApi } from "./helper"
 
 const Discover = () => {
+    let i = 0
+    const [places, setPlaces] = useState([])
+    const [itineraries, setItineraries] = useState([])
+
+    useEffect(() => {
+        call()
+    }, [])
+
+    const call = async () => {
+        const response = await callToApi();
+        setPlaces(response[0])
+        setItineraries(response[0])
+        console.log(response)
+    }
 
     return (
         <section className="discover--main">
             <div className="discover--slot">
-                <Carrousel />
+                <Carrousel objects={places} />
             </div>
             <div className="discover--slot">
-                <Carrousel />
-            </div>
-            <div className="discover--slot">
-                <Carrousel />
+                <Carrousel objects={itineraries} />
             </div>
         </section>
     )
