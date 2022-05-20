a42a0895b574c806763c426dc77bd2279e8638a5
Miguel Cid <miguel.cid.paz@gmail.com>
2022-05-21 00:10:30 +0200

    fix refresh function delete

--- src/components/ListItems/Lists/index.jsx
+++ src/components/ListItems/Lists/index.jsx
@@ -26,17 +26,32 @@ export default function Lists(elements) {
 
   const deleteItem = (id) => {
     elements.title === 'Itineraries' ? deleteItinerary(id) : deletePlace(id);
+    if (elements.title === 'Itineraries') {
+      deleteItinerary(id)
+      const filter = arrayItineraries.filter(e => e.id !== id)
+      window.localStorage.setItem('itinerary', JSON.stringify(filter));
+      setArrayItineraries(filter);
+    } else {
+      deletePlace(id)
+      const filter = arrayPlaces.filter(e => e.id !== id)
+      window.localStorage.setItem('itinerary', JSON.stringify(filter));
+      setArrayPlaces(filter);
+    }
   }
 
   useEffect(() => {
   }, [])
 
+  useEffect(() => {
+
+  }, [arrayPlaces, arrayItineraries])
 
   return (
     <div className="list--container">
       <div className="list--title"><h2>{elements.title}</h2><div className="list--button" onClick={() => navigate(`${elements.path}`)}><AddCircleRounded className="list--icon-add" /></div></div>
       <section className="profileinfo--list">
         {elements.elements.map((element, index) => {
+          console.log(elements.title)
           return (
             <div className="list--row" key={index}>
               <img className="list--img-cover" src={element.image_path} />
@@ -44,7 +59,7 @@ export default function Lists(elements) {
                 <p className="list--name" onClick={viewItem}>{element.name}</p>
               </div>
               <div className="list--buttons">
-                <div className="list--button" onClick={() => modifyItem(index)}><SettingsRounded /></div>
+                {elements.title === 'Places' ? null : <div className="list--button" onClick={() => modifyItem(index)}><SettingsRounded /></div>}
                 <div className="list--button" onClick={() => deleteItem(element.id)}><DeleteRounded /></div>
               </div>
             </div>)
